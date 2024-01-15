1ba64261b708c7bc5866f50119db63050bc6d069
AH-SH <shaabanahmad02@gmail.com>
2024-01-15 03:13:12 +0200

    Inital commit

---
+++ Pages/Products.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TWebApplication1.Pages
+{
+    public class ProductsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
